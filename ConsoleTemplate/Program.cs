203f3cab87fc9e1ffc910662bd6d36846c14b398
Joshua Bagwell <joshua.d.bagwell@gmail.com>
2020-10-13 16:15:23 -0400

    Add project files.

---
+++ ConsoleTemplate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericInfo gi = new GenericInfo();
+            gi.ContactName = "Josh";
+            gi.PhoneNumber = "7706859926";
+            gi.EmailAddress = "fu@fukyou.com";
+            gi.CaseNumber = "CASE-23456";
+            gi.Problem = "Main issue";
+            gi.ProblemDetails = "More information about issue";
+            gi.TroubleShootingSteps = "What I did to troubleshoot the issue";
+            gi.ResolutionDetails = "What the solution was";
+
+            Console.WriteLine(gi.ToString());
+        }
+    }
+}
