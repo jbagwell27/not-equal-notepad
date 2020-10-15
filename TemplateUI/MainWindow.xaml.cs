244d32a78cf5f2269e0a1c07d9a7d7a63c093b5d
Joshua Bagwell <joshua.d.bagwell@gmail.com>
2020-10-14 23:40:45 -0400

    Installed mah nuget

--- TemplateUI/MainWindow.xaml.cs
+++ TemplateUI/MainWindow.xaml.cs
@@ -13,13 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TemplateLogic;
+using MahApps.Metro.Controls;
 
 namespace TemplateUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
